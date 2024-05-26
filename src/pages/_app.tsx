f30320e5e315453047fbb83d824db7af8516504f
Christian A. Smith Mantas <chrsmith@theworkshop.com>
2024-05-26 23:30:19 +0200

    Update: basic now-playing mockup view.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
+// Module imports
 import type { AppProps } from "next/app";
+
+// Global style imports
+import "../styles/globals.scss";
+import "../styles/styles.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
---
+++ src/pages/now-playing/index.tsx
@@ -0,0 +1,76 @@
+// Style imports
+//import './styles.scss';
+
+// now-playing page declaration
+export default function NowPlaying({
+
+}) {
+
+  return (
+    <div className="app-container now-playing">
+
+      {/* Album container */}
+      <div className="information-container">
+
+        {/* Artwork section*/}
+        <div className="artwork-section">
+          <div className="artwork-styled">
+            <img src="/ram-album-cover.jpeg" alt="Album art cover" />
+          </div>
+        </div>
+
+        {/* Information section */}
+        <div className="info-section">
+          <p className="album-title">Random Access Memories</p>
+          <p className="song-title">Lose Yourself to Dance</p>
+          <p className="album-artist">Daft Punk</p>
+        </div>
+
+      </div>
+
+      {/* Player controls section */}
+      <div className="playerControls-styled">
+
+        {/* Scrubber section */}
+        <div className="scrubber-styled">
+          <div className="scrubber-progress" style={{ width: '70%' }} />
+        </div>
+
+        {/* Buttons section */}
+        <div className="buttons-container">
+
+          <div className="button-styled">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+            </svg>
+          </div>
+
+          <div className="button-styled">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z" />
+            </svg>
+          </div>
+
+          <div className="button-styled">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+            </svg>
+          </div>
+
+          <div className="button-styled">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z" />
+            </svg>
+          </div>
+
+          <div className="button-styled">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
